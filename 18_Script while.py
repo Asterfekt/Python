58d5502ddd34558f0beefded98cfe67691ad1a0a
Mateusz Krzyściak <mateuszkrzysciak1@gmail.com>
2023-03-24 15:11:56 +0100

    24.03

---
+++ 18_Script while.py
@@ -0,0 +1,28 @@
+#1
+i=1
+while i<=31:
+    print("ROW NUMBER",i)
+    i+=1
+#2
+kwadrat=1
+while kwadrat<=13:
+    print("kwadrat",kwadrat,":",kwadrat**2)
+    print("szescian",kwadrat,":",kwadrat**3)
+    kwadrat+=1
+#3
+
+dwojka=0
+while dwojka<=16:
+     print(2**dwojka)
+     dwojka+=1
+#4
+start = 1
+end   = 10
+
+number = start
+
+while number<=end:
+
+    print(number*'x')
+
+    number+=1
