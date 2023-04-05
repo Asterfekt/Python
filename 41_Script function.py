c87d3f4eff4bbf167f787e6b0376407a6cc2c270
Mateusz Krzyściak <mateuszkrzysciak1@gmail.com>
2023-04-05 15:25:57 +0200

    05.04

---
+++ 41_Script function.py
@@ -0,0 +1,35 @@
+def PrintCat():
+    #wyświetlającą kota
+    txt = r'''
+    |\---/|
+    | o_o |
+     \_^_/'''
+    print(txt)
+    return
+def PrintBear():
+    #wyświetlającą misia
+    txt = r'''
+    /  \.-"""-./  \
+    \    -   -    /
+     |   o   o   |
+     \  .-'"'-.  /
+      '-\__Y__/-'
+         `---`'''
+    print(txt)
+    return
+def PrintBat():
+    #wyświetlającą nietoperza.
+    txt = r'''
+       /\                 /\
+      / \'._   (\_/)   _.'/ \
+     /_.''._'--('.')--'_.''._\
+     | \_ / `;=/ " \=;` \ _/ |
+      \/ `\__|`\___/`|__/`  \/
+              \(/|\)/     '''
+    print(txt)
+    return
+
+PrintCat()
+PrintBear()
+PrintBat()
+ 
